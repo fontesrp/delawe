895fa0cadab33efa3a8f620ea5ae80c209690344
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-03-24 14:37:20 -0700

    Remove recipes example

--- app/app/reducers/index.js
+++ app/app/reducers/index.js
@@ -3,14 +3,12 @@ import { combineReducers } from "redux";
 import couriers from "./couriers";
 import nav from "./navigation";
 import orders from "./orders";
-import recipes from "./recipes";
 import user from "./user";
 
 const AppReducer = combineReducers({
     couriers,
     nav,
     orders,
-    recipes,
     user
 });
 
