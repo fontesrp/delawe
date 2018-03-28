b00252a4ffb89b7c3dd6865368161fb2a34908fc
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-03-27 20:01:52 -0700

    Revert "Link react-native-push-notification to porject"
    This reverts commit 0c359f7f59f7baf57f82fea97de17200eb856ad9.

--- app/android/app/src/main/java/com/app/MainApplication.java
+++ app/android/app/src/main/java/com/app/MainApplication.java
@@ -3,7 +3,6 @@ package com.app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
             new RNFSPackage(),
             new ImageResizerPackage(),
             new ImagePickerPackage(),
