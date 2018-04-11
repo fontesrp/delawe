8017608c5f26027b4a46eb7fbbea57848e4d8561
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-04-11 14:43:11 -0700

    Update API URL

--- app/app/lib/actionCable.js
+++ app/app/lib/actionCable.js
@@ -1,6 +1,6 @@
 import ActionCable from "action-cable-react-jwt";
 
-const host = "localhost:3000";
+const host = "delawe.rfapps.co";
 
 const createCable = function (jwt) {
     return ActionCable.createConsumer(`ws://${host}/cable`, jwt);
